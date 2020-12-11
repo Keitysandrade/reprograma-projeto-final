ab76b45ef163bc4c8c928d98212837a79e06dcea
Keitysandrade <69424119+Keitysandrade@users.noreply.github.com>
2020-12-10 22:03:20 -0300

    atualizando delete

--- src/controllers/pontosController.js
+++ src/controllers/pontosController.js
@@ -3,87 +3,96 @@ const ponto = require("../models/pontos.js")
 //funcionando
 const getAllPontos = (req, res) => {
 
-console.log(req.url);
- ponto.find(function(err, resultado){
-    if(err) { 
-      res.status(500).send({ message: err.message })
-    }
-    res.status(200).send(resultado);
-  })
+    console.log(req.url);
+    ponto.find(function (err, resultado) {
+        if (err) {
+            res.status(500).send({ message: err.message })
+        }
+        res.status(200).send(resultado);
+    })
 };
 
 
-//não está funcionando
+//funcionando
 const getByResiduo = (req, res) => {
     const tipoDeResiduo = req.query.tipoDeResiduo;
-    console.log (tipoDeResiduo)
- ponto.find({ tipoDeResiduo: tipoDeResiduo}, function(err, resposta){
-   if (err ){
-    res.status(500).send({message: err.message,
-        message: "Erro"});
-   }
-    if (resposta.length> 0){
+    console.log(tipoDeResiduo)
+    ponto.find({ tipoDeResiduo: tipoDeResiduo }, function (err, resposta) {
+        if (err) {
+            res.status(500).send({
+                message: err.message,
+                message: "Erro"
+            });
+        }
+        if (resposta.length > 0) {
 
-        res.status(200).send(resposta)
-    }
-    else 
-{
-res.status(500).send({ message: "Tipo de Residuo não existe"})
-}
-});
+            res.status(200).send(resposta)
+        }
+        else {
+            res.status(500).send({ message: "Tipo de Residuo não existe" })
+        }
+    });
 };
 
 
 //funcionando
 const createPonto = (req, res) => {
-    console.log (req.body)
+    console.log(req.body)
     let pontoaCriar = new ponto(req.body)
 
-    pontoaCriar.save(function(err){
+    pontoaCriar.save(function (err) {
         res.status(200).send(pontoaCriar.toJSON())
-   })
+    })
 
-      };
+};
 
-      //funcionando
+//Não está funcionando
 const updatePonto = (req, res) => {
     const id = req.params.id;
 
-    ponto.find({ id }, function(err, resultado){
-      if(resultado.length> 0){
-                
-        ponto.updateMany({ id }, { $set : req.body }, function (err) {
-          if (err) {
-            res.status(500).send({ message: err.message })
-          }
-          res.status(200).send({ message: "Ponto alterado com sucesso"})
-        })
-      }else {
-        res.status(500).send({ message: "Não há registros para serem atualizados com esse id"})
-      }
-    })
-  
-  }
-  
-//não está funcionando
-const deletePonto = (req, res) => {
+    ponto.find({ id }, function (err, resultado) {
+        if (resultado.length > 0) {
 
-     const id = req.query.id;
-    ponto.deleteMany({ id }), function (err){
-        if(err) {
-            res.status(500).send({ message: err.message })
-        }else {
-            res.status(200).send({ message: "Ponto removido com sucesso"})
+            ponto.updateMany({ id }, { $set: req.body }, function (err) {
+                if (err) {
+                    res.status(500).send({ message: err.message })
+                }
+                res.status(200).send({ message: "Ponto alterado com sucesso" })
+            })
+        } else {
+            res.status(500).send({ message: "Não há registros para serem atualizados com esse id" })
         }
-    }
+    })
+
 }
+
+// funcionando
+const deletePonto = (req, res) => {
+    const id = req.params.id;
+    ponto.find({id:id} , function (err, resposta) {
+        console.log (resposta)
+        if (resposta) {
+            ponto.deleteOne({ id }, function (err) {
+                if (err) {
+                    res.status(500).send({ message: err.message })
+                }
+                res.status(200).send({ message: "Ponto excluido com sucesso!" })
+            })
+}
+    else {
+        res.status(200).send({ message: "Não há ponto para ser excluido" })
+        };
+    });
+};
+
+
 
 
 
 module.exports = {
     getAllPontos,
     getByResiduo,
-    createPonto, 
+    createPonto,
     updatePonto,
     deletePonto
 
